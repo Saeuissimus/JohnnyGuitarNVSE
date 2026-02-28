bc63189a1d59fd3f343e37d2d0db8051e823a239
Saeuissimus <nalus_saeuus@protonmail.com>
2026-02-27 23:23:21 -0300

    adds more debug messages to event framework

--- JG/events/EventFramework.cpp
+++ JG/events/EventFramework.cpp
@@ -10,9 +10,13 @@ void* __fastcall GenericCreateFilter(void** Filters, uint32_t numFilters) {
 }
 
 EventInfo __cdecl JGCreateEvent(const char* EventName, uint8_t maxArgs, uint8_t maxFilters, void* (__fastcall* CreatorFunction)(void**, uint32_t)) {
+	_MESSAGE("JohnnyGuitarNVSE JGCreateEvent.");
 	std::lock_guard<std::mutex> lock(eventInfosMutex);
+	_MESSAGE("JohnnyGuitarNVSE JGCreateEvent mutex acquired.");
 	EventInfo eventinfo = new EventInformation(EventName, maxArgs, maxFilters, CreatorFunction);
+	_MESSAGE("JohnnyGuitarNVSE JGCreateEvent event information created.");
 	EventInfos.push_back(eventinfo);
+	_MESSAGE("JohnnyGuitarNVSE JGCreateEvent event information pushed.");
 	return eventinfo;
 }
 
